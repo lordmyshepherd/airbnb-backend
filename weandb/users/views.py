57a34315ac70b32639281b2d16e86071c70a4730
lordmyshepherd <lordmyshepherd29@gmail.com>
2019-11-12 03:58:52 +0900

    Sign_in 엔드포인트

--- weandb/users/views.py
+++ weandb/users/views.py
@@ -1,19 +1,33 @@
 import json
 import jwt
 import bcrypt
-from django.http    import JsonResponse
-from django.views   import View
-from .models        import Users
+from django.http        import JsonResponse
+from django.views       import View
+from .models            import Users
+from weandb.settings    import SECRET_KEY
 
 class SignUpView(View) :
     def post(self, request) :
-        data            = json.loads(request.body)
-        password        = data["password"]
-        hashed_password = bcrypt.hashpw(b"password", bcrypt.gensalt())
-
+        credential      = json.loads(request.body)
+        hashed_password = bcrypt.hashpw(credential["password"].encode('utf-8'), bcrypt.gensalt())
         Users(
-            email       = data["email"],
+            email       = credential["email"],
             password    = hashed_password.decode("utf-8")
         ).save()
 
         return JsonResponse({"MESSAGE":"SUCCESS"}, status=200)
+
+class SignInView(View) :
+    def post(self, request) :
+        credential  = json.loads(request.body)
+
+        try :
+            user = Users.objects.get(email=credential["email"])
+            if bcrypt.checkpw(credential["password"].encode("utf-8"), user.password.encode("utf-8")) :
+                payload             = {"user_id" : user.id}
+                encryption_secret   = SECRET_KEY
+                algorithm           = "HS256"
+                encoded             = jwt.encode(payload, encryption_secret, algorithm=algorithm)
+                return JsonResponse({"access_token":encoded.decode("utf-8")}, status=200)
+        except :
+            return JsonResponse({"message":"INVALID_EMAIL"}, status=400)
